d64b4889d5c8c9ff55af6574d80eff3c32bb9ebb
Louie Schoenknecht <luisesch97@gmail.com>
2021-03-26 15:00:00 -0700

    run identity migrations

--- IdentityBakery/Migrations/20210326215137_addIdentity.Designer.cs
+++ IdentityBakery/Migrations/20210326215137_addIdentity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using IdentityBakery.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityBakery.Migrations
 {
     [DbContext(typeof(IdentityBakeryContext))]
-    partial class IdentityBakeryContextModelSnapshot : ModelSnapshot
+    [Migration("20210326215137_addIdentity")]
+    partial class addIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
