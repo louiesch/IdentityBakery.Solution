f0a8ac9620030ac97df600cb2947a15239ef8dae
Louie Schoenknecht <luisesch97@gmail.com>
2021-03-26 11:33:49 -0700

    add ability to add flavor to a treat from the treats view

--- IdentityBakery/Controllers/TreatsController.cs
+++ IdentityBakery/Controllers/TreatsController.cs
@@ -67,5 +67,23 @@ namespace IdentityBakery.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+        if (FlavorId != 0)
+        {
+        _db.TreatFlavor.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId });
+        }
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
   }
 }
