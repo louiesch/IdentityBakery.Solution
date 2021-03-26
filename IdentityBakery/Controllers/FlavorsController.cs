c7ad90d27b1fb747e403590b798a47154c662c6f
Louie Schoenknecht <luisesch97@gmail.com>
2021-03-26 12:47:38 -0700

    add flavor create controller route and view

--- IdentityBakery/Controllers/FlavorsController.cs
+++ IdentityBakery/Controllers/FlavorsController.cs
@@ -21,5 +21,24 @@ namespace IdentityBakery.Controllers
       List<Flavor> model = _db.Flavors.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor, int TreatId)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      if (TreatId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() {TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
